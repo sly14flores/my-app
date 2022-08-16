4fdb9568b0e7f34291915ccf4f78d162eedeeb30
Sly Flores <sly@christian.com.ph>
2022-08-16 17:00:05 +0800

    Update index.js

--- src/pages/Groups/index.js
+++ src/pages/Groups/index.js
@@ -1,6 +1,9 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Outlet, useOutlet, useLocation } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
+
+import axios from 'axios'
+import { apiUrl } from "../../constants/url"
 
 import List from "./List"
 
@@ -14,6 +17,12 @@ const Groups = () => {
   const [records, setRecords] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
   const [pagination, setPagination] = useState({})
+
+  useEffect(() => {
+    axios.get(`${apiUrl}/sanctum/csrf-cookie`).then(response => {
+      console.log(response)
+    });
+  },[])
 
   const results = useQuery(['groupsList', currentPage, filters], () => getRecords({ page: currentPage, filters }), {
     keepPreviousData : true,
